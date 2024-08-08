ab3d62d30ac752fe599e5232b22a30248bf963ce
rohan <rohanthakur89768@gmail.com>
2024-08-08 16:45:35 +0530

    added how to upload file on cloudarnary

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+
+cloudinary.config({ 
+    cloud_name:  process.env.CLOUDNARY_NAME,
+    api_key:  process.env.CLOUDNARY_API,
+    api_secret: process.env.CLOUDNARY_API_SECRET
+});
+
+
+const uploadonCLoudnary = async (localfilepath) =>{
+          try {
+            if(!localfilepath) return null;
+
+            const response = await cloudinary.uploader.upload(localfilepath,{
+                resource_type : "auto"
+            })
+            console.log("file uploaded succesfully on cloudnary", response.url)
+             fs.unlinkSync(localfilepath)
+             return response;
+
+          } catch (error) {
+              fs.unlinkSync(localfilepath)
+              return null
+          }
+}
+
+export {uploadonCLoudnary}
