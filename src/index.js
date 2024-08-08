c72d49a19ed534f4d1d40f92789f3369c207c73d
rohan <rohanthakur89768@gmail.com>
2024-08-08 09:53:28 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectdb from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : "./env"
 })
 
-connectdb();
+connectdb()
+.then(() =>{
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server is up and running on ${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log("DB Connection failed ..");
+})
